88ecebc5335c9b5d6bd5be3202266f5e5de27309
Udson Silva <udson2113@outlook.com>
2022-12-30 11:21:26 -0300

    Logica para exibir entradas.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,5 @@
+from markdown2 import markdown
+
 from django.shortcuts import render
 
 from . import util
@@ -8,3 +10,20 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, entry_title):
+    content = util.get_entry(entry_title)
+
+    if content is None:
+        return entry_not_found(request, entry_title)
+    
+    return render(request, "encyclopedia/entry.html", {
+        "title": entry_title,
+        "content": markdown(content)
+    })
+
+
+def entry_not_found(request, entry_title):
+    return render(request, "encyclopedia/entry_not_found.html", {
+        "title": entry_title
+    })

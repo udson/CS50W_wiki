b399fd033a0e075f6f5d71b67d5bfb751e7f4d03
Udson Silva <udson2113@outlook.com>
2023-01-02 11:57:05 -0300

    Ajustes finais.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("random", views.random_entry, name="random"),
     path("search", views.search, name="search"),
-    path("wiki/new_entry", views.new_entry, name="new_entry"),
-    path("wiki/edit_entry/<str:entry_title>", views.edit_entry, name='edit_entry'),
+    path("new", views.new_entry, name="new"),
+    path("edit/<str:entry_title>", views.edit_entry, name='edit'),
     path("wiki/<str:entry_title>", views.entry, name="entry"),
 ]

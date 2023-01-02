2a53f27c728553d4c071c71659983281771435a0
Udson Silva <udson2113@outlook.com>
2023-01-01 21:54:34 -0300

    Implemetando página para nova entrada.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("random", views.random_entry, name="random"),
     path("search", views.search, name="search"),
+    path("wiki/new_entry", views.new_entry, name="new_entry"),
     path("wiki/<str:entry_title>", views.entry, name="entry"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from . import util
+from .forms import EntryForm
 
 
 def index(request):
@@ -45,3 +46,23 @@ def search(request):
 
 def random_entry(request):
     return HttpResponseRedirect(reverse('entry', args=[choice(util.list_entries())]))
+
+
+def new_entry(request):
+    if request.method == "POST":
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            if util.get_entry(title) is None:
+                util.save_entry(title, content)
+                return HttpResponseRedirect(reverse(
+                    'entry', args=[request.POST['title']]
+                ))
+            form.add_error(field=None, error=f'Error! The entry "{title}" already exists.')
+    else:
+        form = EntryForm()
+
+    return render(request, "encyclopedia/new_entry.html", {
+        "form": form,
+    })
